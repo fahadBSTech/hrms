861e099573f05dbcb037dbb131eeee30244ced07
Debian <bitnami@ip-172-31-88-16.ec2.internal>
2024-07-25 05:32:31 +0000

    added custom attendance validations

--- hrms/utils/holiday_list.py
+++ hrms/utils/holiday_list.py
@@ -13,68 +13,69 @@ def is_holiday(holiday_list, date):
 
 	return bool(holidays)
 
-
 @frappe.whitelist()
-def get_current_month_working_days(company):
-	user = frappe.session.user
-	employee = frappe.get_value("Employee", {"user_id": user}, "name")
+def get_current_month_working_days(company, start_date, end_date):
+    user = frappe.session.user
+    employee = frappe.get_value("Employee", {"user_id": user}, "name")
 
-	if not employee:
-		frappe.throw("Employee record not found for the logged-in user")
+    if not employee:
+        frappe.throw("Employee record not found for the logged-in user")
 
-	today = getdate(nowdate())
-	start_date = get_first_day(today)
-	end_date = get_last_day(today)
-	holiday_list = frappe.get_value("Company", company, "default_holiday_list")
+    today = getdate(nowdate())
+    start_date = getdate(start_date)
+    end_date = getdate(end_date)
+    holiday_list = frappe.get_value("Company", company, "default_holiday_list")
 
-	total_working_days_count = 0
-	current_date = start_date
+    total_working_days_count = 0
+    current_date = start_date
 
-	while current_date <= end_date:
-		if not is_holiday(holiday_list,
-						  current_date) and current_date.weekday() < 5:  # Count weekdays and non-holidays as working days
-			total_working_days_count += 1
-		current_date = add_days(current_date, 1)
+    while current_date <= end_date:
+        if not is_holiday(holiday_list,
+                          current_date) and current_date.weekday() < 5:  # Count weekdays and non-holidays as working days
+            total_working_days_count += 1
+        current_date = add_days(current_date, 1)
 
-	leaves = frappe.get_all('Leave Application',
-							filters={
-								'employee': employee,
-								'status': 'Approved',
-								'from_date': ('<=', today),
-								'to_date': ('>=', start_date)
-							},
-							fields=['from_date', 'to_date', 'half_day'])
+    leaves = frappe.get_all('Leave Application',
+                            filters={
+                                'employee': employee,
+                                'status': 'Approved',
+                                'from_date': ('<=', today),
+                                'to_date': ('>=', start_date)
+                            },
+                            fields=['from_date', 'to_date', 'half_day'])
 
-	leave_days_count = 0
-	for leave in leaves:
-		leave_start = getdate(leave['from_date'])
-		leave_end = getdate(leave['to_date'])
-		current_leave_date = leave_start
+    leave_days_count = 0
+    for leave in leaves:
+        leave_start = getdate(leave['from_date'])
+        leave_end = getdate(leave['to_date'])
+        current_leave_date = leave_start
 
-		while current_leave_date <= leave_end:
-			if start_date <= current_leave_date <= today:
-				if leave['half_day']:
-					leave_days_count += 0.5
-				else:
-					leave_days_count += 1
-			current_leave_date = add_days(current_leave_date, 1)
+        while current_leave_date <= leave_end:
+            if start_date <= current_leave_date <= today:
+                if leave['half_day']:
+                    leave_days_count += 0.5
+                else:
+                    leave_days_count += 1
+            current_leave_date = add_days(current_leave_date, 1)
 
-	working_days_till_today = 0
-	current_date = start_date
+    working_days_till_today = 0
+    current_date = start_date
 
-	while current_date <= today:
-		if not is_holiday(holiday_list,
-						  current_date) and current_date.weekday() < 5:  # Count weekdays and non-holidays as working days
-			working_days_till_today += 1
-		current_date = add_days(current_date, 1)
+    while current_date <= today:
+        if not is_holiday(holiday_list,
+                          current_date) and current_date.weekday() < 5:  # Count weekdays and non-holidays as working days
+            working_days_till_today += 1
+        current_date = add_days(current_date, 1)
 
-	actual_working_days_count = working_days_till_today - leave_days_count
+    actual_working_days_count = working_days_till_today - leave_days_count
 
-	return {
-		'total_working_days': total_working_days_count,
-		'off_days': leave_days_count,
-		'employee_working_days': actual_working_days_count
-	}
+    return {
+        'total_working_days': total_working_days_count,
+        'off_days': leave_days_count,
+        'employee_working_days': actual_working_days_count
+    }
+
+
 
 def get_holiday_dates_between(
 	holiday_list: str,
