bdc11157932462d4ab05e031d1362802c1905b3f
Debian <bitnami@ip-172-31-18-151.ec2.internal>
2024-08-21 08:31:54 +0000

    Added doctype and report changes

--- hrms/hr/doctype/shift_type/shift_type.py
+++ hrms/hr/doctype/shift_type/shift_type.py
@@ -292,6 +292,7 @@ class ShiftType(Document):
 def process_auto_attendance_for_all_shifts():
     shift_list = frappe.get_all("Shift Type", filters={"enable_auto_attendance": "1"}, pluck="name")
     for shift in shift_list:
+        print("Shift Type:", shift)
         doc = frappe.get_cached_doc("Shift Type", shift)
         doc.process_auto_attendance()
 
---
+++ hrms/hr/notification/wfh_feedback_form/wfh_feedback_form.md
@@ -0,0 +1,12 @@
+Work From Home Feedback Required:
+
+Details:
+Employee
+{{employee_name}}
+From Date
+{{from_date}}
+To Date
+{{to_date}}
+
+
+Click the Following link to Submit your WFH Feedback form https://docs.google.com/forms/d/e/1FAIpQLSeahNxC4HBTgRn2w732uBbWhTFoJYD3JM1-2B79sTudRAO4hQ/viewform
